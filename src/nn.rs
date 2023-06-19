fe7fd7fba688c0acc8bb81bc509109d18af1b31c
Teddy Rendahl <teddy@osaro.com>
2023-06-18 20:07:46 -0700

    Implement Neural Network (#1)
    * WIP: Layers
    
    * Neural Net implementation

---
+++ src/nn.rs
@@ -0,0 +1,200 @@
+use rand::{thread_rng, Rng};
+use std::ops::Add;
+
+use crate::value::Value;
+
+#[derive(Debug)]
+struct Neuron<const N: usize> {
+    weights: [Value; N],
+    bias: Value,
+}
+
+impl<const N: usize> Neuron<N> {
+    fn new() -> Self {
+        let mut rng = thread_rng();
+        Neuron {
+            weights: (0..N)
+                .map(|_| Value::from(rng.gen_range(-1.0..1.0)))
+                .collect::<Vec<Value>>()
+                .try_into()
+                .unwrap(),
+            bias: Value::from(0.),
+        }
+    }
+
+    fn forward(&self, x: [Value; N]) -> Value {
+        self.weights
+            .clone()
+            .into_iter()
+            .zip(x.into_iter())
+            .map(|(a, b)| a * b)
+            .sum::<Value>()
+            .add(self.bias.clone())
+            .tanh()
+    }
+
+    fn parameters(&self) -> Vec<Value> {
+        let mut p = self.weights.clone().to_vec();
+        p.push(self.bias.clone());
+        p
+    }
+}
+
+pub trait Layer {
+    fn forward(&self, x: Vec<Value>) -> Vec<Value>;
+    fn parameters(&self) -> Vec<Value>;
+}
+
+/// A Layer with the input and output dimensions as generics
+pub struct SizedLayer<const I: usize, const O: usize> {
+    neurons: [Neuron<I>; O],
+}
+
+impl<const I: usize, const O: usize> Default for SizedLayer<I, O> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<const I: usize, const O: usize> SizedLayer<I, O> {
+    /// Create a layer of the provided size, initialized with random weights
+    pub fn new() -> Self {
+        Self {
+            neurons: (0..O)
+                .map(|_| Neuron::new())
+                .collect::<Vec<Neuron<I>>>()
+                .try_into()
+                .unwrap(),
+        }
+    }
+}
+
+impl<const I: usize, const O: usize> Layer for SizedLayer<I, O> {
+    fn forward(&self, x: Vec<Value>) -> Vec<Value> {
+        self.neurons
+            .iter()
+            .map(|n| n.forward(x.clone().try_into().unwrap()))
+            .collect::<Vec<Value>>()
+    }
+
+    fn parameters(&self) -> Vec<Value> {
+        self.neurons.iter().flat_map(|n| n.parameters()).collect()
+    }
+}
+
+pub struct Mlp<const I: usize, const O: usize> {
+    layers: Vec<Box<dyn Layer>>,
+}
+
+impl<const I: usize, const O: usize> Mlp<I, O> {
+    /// Create a new Mlp from an initial layer
+    pub fn from_layer(layer: SizedLayer<I, O>) -> Mlp<I, O> {
+        Self {
+            layers: vec![Box::new(layer)],
+        }
+    }
+
+    /// Add a layer to the Mlp
+    ///
+    /// Consumes the current Mlp re-defining the type to have the same number
+    /// of outputs as the added layer.
+    pub fn add_layer<const OUT: usize>(mut self, layer: SizedLayer<O, OUT>) -> Mlp<I, OUT> {
+        self.layers.push(Box::new(layer));
+        Mlp {
+            layers: self.layers,
+        }
+    }
+
+    /// Create a prediction by evaluating an input through a forward pass of each layer
+    pub fn forward(&self, x: [Value; I]) -> [Value; O] {
+        let mut x = x.to_vec();
+        for layer in &self.layers {
+            x = layer.forward(x)
+        }
+        x.try_into().unwrap()
+    }
+
+    /// Complete list of parameters in the Mlp graph
+    pub fn parameters(&self) -> Vec<Value> {
+        self.layers.iter().flat_map(|l| l.parameters()).collect()
+    }
+
+    /// Set all parameter gradients back to zero
+    pub fn zero_grad(&self) {
+        for p in self.parameters() {
+            p.zero_grad()
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::nn::{Layer, Mlp, SizedLayer};
+    use crate::value::Value;
+    use rstest::{fixture, rstest};
+
+    #[test]
+    fn test_layer_forward() {
+        let l: SizedLayer<2, 3> = SizedLayer::new();
+        let o = l.forward([2.0, 3.0].into_iter().map(Value::from).collect());
+        assert_eq!(o.len(), 3);
+    }
+
+    #[fixture]
+    fn mlp() -> Mlp<3, 1> {
+        Mlp::from_layer(SizedLayer::<3, 4>::new())
+            .add_layer(SizedLayer::<4, 4>::new())
+            .add_layer(SizedLayer::new())
+    }
+
+    #[rstest]
+    fn test_mlp_forward(mlp: Mlp<3, 1>) {
+        let o = mlp.forward([Value::from(2.0), Value::from(3.0), Value::from(-1.0)]);
+        assert_eq!(o.len(), 1);
+    }
+
+    #[rstest]
+    fn test_mlp_parameters(mlp: Mlp<3, 1>) {
+        let p = mlp.parameters();
+        assert_eq!(p.len(), 41);
+    }
+
+    #[rstest]
+    fn test_mpl_train(mlp: Mlp<3, 1>) {
+        let dataset = [
+            [Value::from(2.0), Value::from(3.0), Value::from(-1.0)],
+            [Value::from(3.0), Value::from(-1.0), Value::from(0.5)],
+            [Value::from(0.5), Value::from(1.0), Value::from(1.0)],
+            [Value::from(1.0), Value::from(1.0), Value::from(-1.0)],
+        ];
+
+        let truth = [
+            Value::from(1.0),
+            Value::from(-1.0),
+            Value::from(-1.0),
+            Value::from(1.0),
+        ];
+
+        let mut last_loss: Option<f64> = None;
+        for _ in 0..15 {
+            let loss: Value = dataset
+                .clone()
+                .into_iter()
+                .zip(truth.clone().into_iter())
+                .map(|(d, t)| (t - mlp.forward(d)[0].clone()).powi(2))
+                .sum();
+
+            mlp.zero_grad();
+            loss.backward();
+            for p in mlp.parameters() {
+                p.set_data(p.data() + p.gradient() * -0.1)
+            }
+            if let Some(ls) = last_loss {
+                if ls < loss.data() {
+                    panic!("Loss got worse!")
+                }
+            }
+            last_loss = Some(loss.data())
+        }
+    }
+}
---
+++ src/value.rs
@@ -0,0 +1,365 @@
+use std::{
+    cell::RefCell,
+    collections::HashSet,
+    fmt::Debug,
+    fmt::Display,
+    hash::Hash,
+    iter::Sum,
+    ops::{Add, Deref, Div, Mul, Sub},
+    rc::Rc,
+};
+
+#[derive(Debug, Hash, Clone, Copy, PartialEq, Eq)]
+pub enum Operation {
+    Add,
+    Multiply,
+    Tanh,
+    Exponent,
+    Pow(i32),
+}
+
+/// Implementation of an equation value
+///
+/// Actual is kept internally so that Value can be freely cloned
+/// without actually copying data
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Value(Rc<RefCell<ValueInternal>>);
+
+/// Internal holder of Value information
+pub struct ValueInternal {
+    pub data: f64,
+    pub children: Vec<Value>,
+    pub operation: Option<Operation>,
+    pub gradient: f64,
+    pub label: Option<String>,
+}
+
+impl Deref for Value {
+    type Target = Rc<RefCell<ValueInternal>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Hash for Value {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.borrow().hash(state)
+    }
+}
+
+impl Debug for ValueInternal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ValueInternal")
+            .field("data", &self.data)
+            .field("gradient", &self.gradient)
+            .field("label", &self.label)
+            .field("operation", &self.operation)
+            .field("children", &self.children.len())
+            .finish()
+    }
+}
+
+impl Value {
+    fn new(value: ValueInternal) -> Self {
+        Value(Rc::new(RefCell::new(value)))
+    }
+
+    /// Access the internal f64 of the Value
+    pub fn data(&self) -> f64 {
+        self.borrow().data
+    }
+
+    /// Set the internal f64 of the Value
+    pub fn set_data(&self, value: f64) {
+        self.borrow_mut().data = value;
+    }
+
+    /// The gradient of the current value
+    ///
+    /// Will be 0. until the value is included as part of the `backward` call
+    pub fn gradient(&self) -> f64 {
+        self.borrow().gradient
+    }
+
+    /// Zero the gradient value
+    pub fn zero_grad(&self) {
+        self.borrow_mut().gradient = 0.;
+    }
+
+    /// The child nodes of the Value
+    pub fn children(&self) -> Vec<Value> {
+        self.borrow().children.clone()
+    }
+
+    /// The operation that created this node (if any)
+    pub fn operation(&self) -> Option<Operation> {
+        self.borrow().operation
+    }
+
+    /// Apply the tanh operation to the node, creating a new Value
+    pub fn tanh(self) -> Value {
+        let d = self.borrow().data.tanh();
+        Value::new(ValueInternal::new(
+            d,
+            vec![self],
+            Some(Operation::Tanh),
+            None,
+        ))
+    }
+
+    /// Apply the exp operation to the node, creating a new Value
+    pub fn exp(self) -> Self {
+        let d = self.borrow().data.exp();
+        Value::new(ValueInternal::new(
+            d,
+            vec![self],
+            Some(Operation::Exponent),
+            None,
+        ))
+    }
+
+    /// Apply the powi operation to the node, creating a new Value
+    pub fn powi(self, value: i32) -> Self {
+        Value::new(ValueInternal::new(
+            self.borrow().data.powi(value),
+            vec![self.clone()],
+            Some(Operation::Pow(value)),
+            None,
+        ))
+    }
+
+    /// Apply backward propagation of the gradient for this Value and all children in our graph
+    pub fn backward(&self) {
+        let mut topo = Vec::new();
+        let mut visited = HashSet::new();
+
+        fn build_topo(node: Value, visited: &mut HashSet<Value>, topo: &mut Vec<Value>) {
+            if !visited.contains(&node) {
+                visited.insert(node.clone());
+                for child in node.children() {
+                    build_topo(child, visited, topo)
+                }
+                topo.push(node)
+            }
+        }
+        build_topo(self.clone(), &mut visited, &mut topo);
+        self.borrow_mut().gradient = 1.0;
+        for node in topo.into_iter().rev() {
+            node.backward_internal()
+        }
+    }
+    fn backward_internal(&self) {
+        match self.operation() {
+            Some(Operation::Add) => {
+                for child in self.children().iter_mut() {
+                    child.borrow_mut().gradient += self.gradient();
+                }
+            }
+            Some(Operation::Multiply) => {
+                let first = self.children()[0].data();
+                let second = self.children()[1].data();
+
+                self.children()[0].borrow_mut().gradient = second * self.gradient();
+                self.children()[1].borrow_mut().gradient = first * self.gradient();
+            }
+            Some(Operation::Tanh) => {
+                for child in self.children().iter_mut() {
+                    child.borrow_mut().gradient += (1.0 - self.data().powi(2)) * self.gradient();
+                }
+            }
+            Some(Operation::Exponent) => {
+                for child in self.children().iter_mut() {
+                    child.borrow_mut().gradient += self.data() * self.gradient()
+                }
+            }
+            Some(Operation::Pow(v)) => {
+                for child in self.children().iter_mut() {
+                    child.borrow_mut().gradient +=
+                        v as f64 * child.data().powf(v as f64 - 1.0) * self.gradient();
+                }
+            }
+            None => (),
+        };
+    }
+}
+
+impl Display for ValueInternal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Value(data={})", self.data)
+    }
+}
+
+impl Hash for ValueInternal {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.data.to_bits().hash(state);
+        self.gradient.to_bits().hash(state);
+        self.label.hash(state);
+        self.operation.hash(state);
+        self.children.hash(state);
+    }
+}
+impl ValueInternal {
+    fn new(
+        data: f64,
+        children: Vec<Value>,
+        operation: Option<Operation>,
+        label: Option<String>,
+    ) -> Self {
+        Self {
+            data,
+            children,
+            operation,
+            gradient: 0.,
+            label,
+        }
+    }
+}
+impl From<f64> for Value {
+    fn from(data: f64) -> Self {
+        Value::new(ValueInternal::new(data, vec![], None, None))
+    }
+}
+
+impl Add for Value {
+    type Output = Value;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Value::new(ValueInternal {
+            data: self.data() + rhs.data(),
+            children: vec![rhs, self],
+            operation: Some(Operation::Add),
+            gradient: 0.,
+            label: None,
+        })
+    }
+}
+
+impl Sub for Value {
+    type Output = Value;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self + rhs * Value::from(-1.0)
+    }
+}
+
+impl Mul for Value {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Value::new(ValueInternal::new(
+            self.data() * rhs.data(),
+            vec![self, rhs],
+            Some(Operation::Multiply),
+            None,
+        ))
+    }
+}
+
+impl Div for Value {
+    type Output = Self;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        self * rhs.powi(-1)
+    }
+}
+
+impl PartialEq for ValueInternal {
+    fn eq(&self, other: &Self) -> bool {
+        self.data == other.data
+            && self.gradient == other.gradient
+            && self.label == other.label
+            && self.operation == other.operation
+            && self.children == other.children
+    }
+}
+impl Sum for Value {
+    fn sum<I: Iterator<Item = Self>>(iter: I) -> Self {
+        let mut sum = Value::from(0.0);
+        for value in iter {
+            sum = sum + value
+        }
+        sum
+    }
+}
+
+impl Eq for ValueInternal {}
+#[cfg(test)]
+mod tests {
+    use crate::value::Value;
+    use approx::assert_abs_diff_eq;
+
+    #[test]
+    fn test_backprop_add_and_mul() {
+        let a = Value::from(2.0);
+        let b = Value::from(-3.0);
+        let c = Value::from(10.0);
+        let e = a.clone() * b.clone();
+        let d = e.clone() + c.clone();
+        let f = Value::from(-2.0);
+        let l = d.clone() * f.clone();
+        l.backward();
+
+        assert_eq!(l.gradient(), 1.0);
+        assert_eq!(d.gradient(), -2.0);
+        assert_eq!(f.gradient(), 4.0);
+        assert_eq!(c.gradient(), -2.0);
+        assert_eq!(e.gradient(), -2.0);
+        assert_eq!(a.gradient(), 6.0);
+        assert_eq!(b.gradient(), -4.0);
+    }
+
+    #[test]
+    fn test_backprop_neuron() {
+        let x1 = Value::from(2.0);
+        let x2 = Value::from(0.0);
+        let w1 = Value::from(-3.0);
+        let w2 = Value::from(1.0);
+        let b = Value::from(6.881_373_587_019_543);
+
+        let x1w1 = x1.clone() * w1.clone();
+        let x2w2 = x2.clone() * w2.clone();
+        let x1w1x2w2 = x1w1.clone() + x2w2.clone();
+        let n = x1w1x2w2 + b.clone();
+        let o = n.clone().tanh();
+        o.backward();
+
+        assert_abs_diff_eq!(o.gradient(), 1.0, epsilon = 0.001);
+        assert_abs_diff_eq!(w1.gradient(), 1.0, epsilon = 0.001);
+        assert_eq!(w2.gradient(), 0.0);
+        assert_abs_diff_eq!(x1.gradient(), -1.5, epsilon = 0.001);
+        assert_abs_diff_eq!(x2.gradient(), 0.5, epsilon = 0.001);
+        assert_abs_diff_eq!(n.gradient(), 0.5, epsilon = 0.001);
+        assert_abs_diff_eq!(b.gradient(), 0.5, epsilon = 0.001);
+        assert_abs_diff_eq!(x2w2.gradient(), 0.5, epsilon = 0.001);
+        assert_abs_diff_eq!(x1w1.gradient(), 0.5, epsilon = 0.001);
+    }
+
+    #[test]
+    fn test_backprop_neuron_from_components() {
+        let x1 = Value::from(2.0);
+        let x2 = Value::from(0.0);
+        let w1 = Value::from(-3.0);
+        let w2 = Value::from(1.0);
+        let b = Value::from(6.881_373_587_019_543);
+
+        let x1w1 = x1.clone() * w1.clone();
+        let x2w2 = x2.clone() * w2.clone();
+        let x1w1x2w2 = x1w1.clone() + x2w2.clone();
+        let n = x1w1x2w2 + b.clone();
+        let e = (Value::from(2.0) * n.clone()).exp();
+        let o = (e.clone() - Value::from(1.0)) / (e + Value::from(1.0));
+        o.backward();
+
+        assert_abs_diff_eq!(o.gradient(), 1.0, epsilon = 0.001);
+        assert_abs_diff_eq!(n.gradient(), 0.5, epsilon = 0.001);
+        assert_abs_diff_eq!(w1.gradient(), 1.0, epsilon = 0.001);
+        assert_eq!(w2.gradient(), 0.0);
+        assert_abs_diff_eq!(x1.gradient(), -1.5, epsilon = 0.001);
+        assert_abs_diff_eq!(x2.gradient(), 0.5, epsilon = 0.001);
+        assert_abs_diff_eq!(n.gradient(), 0.5, epsilon = 0.001);
+        assert_abs_diff_eq!(b.gradient(), 0.5, epsilon = 0.001);
+        assert_abs_diff_eq!(x2w2.gradient(), 0.5, epsilon = 0.001);
+        assert_abs_diff_eq!(x1w1.gradient(), 0.5, epsilon = 0.001);
+    }
+}
